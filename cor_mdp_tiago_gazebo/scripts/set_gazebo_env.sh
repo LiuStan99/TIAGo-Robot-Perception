5d56b04a99fc42756cd22ba1e2516aa4212f74ee
Chadi Salmi <c.salmi@tudelft.nl>
2022-04-20 11:08:38 +0200

    Add coffee cup models with apriltags

--- cor_mdp_tiago_gazebo/scripts/set_gazebo_env.sh
+++ cor_mdp_tiago_gazebo/scripts/set_gazebo_env.sh
@@ -9,3 +9,4 @@ echo "export GAZEBO_MODEL_PATH=${PWD%/*}/models/AH_thee_bosvruchten:\$GAZEBO_MOD
 echo "export GAZEBO_MODEL_PATH=${PWD%/*}/models/AH_thee:\$GAZEBO_MODEL_PATH" >> ~/.bashrc
 echo "export GAZEBO_MODEL_PATH=${PWD%/*}/models/AH_store:\$GAZEBO_MODEL_PATH" >> ~/.bashrc
 echo "export GAZEBO_MODEL_PATH=${PWD%/*}/models/legacy_models:\$GAZEBO_MODEL_PATH" >> ~/.bashrc
+echo "export GAZEBO_MODEL_PATH=${PWD%/*}/models/coffee_cups:\$GAZEBO_MODEL_PATH" >> ~/.bashrc
