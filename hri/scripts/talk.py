856da64608348315045634f4e54372ed7e124109
Stijn Lafontaine <s.c.lafontaine@student.tudelft.nl>
2022-05-16 13:14:36 +0200

    Worked on the talk_node

--- hri/scripts/talk.py
+++ hri/scripts/talk.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
-
-# Copyright (c) 2016 PAL Robotics SL. All Rights Reserved
-#
-# Permission to use, copy, modify, and/or distribute this software for
-# any purpose with or without fee is hereby granted, provided that the
-# above copyright notice and this permission notice appear in all
-# copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-#
-# Author:
-#   * Job van Dieten
-
 #import ROS headers
 import rospy
 import actionlib
@@ -45,15 +25,15 @@ def feedbackCb(feedback):
 	print("-")
 
 if __name__ == '__main__':
-
-	rospy.init_node('tts_client')
+	rospy.init_node('hri_tts_client')
 	client = actionlib.SimpleActionClient('tts_to_soundplay', TtsAction)
 	rospy.loginfo("Waiting for Server")
 	client.wait_for_server()
 	rospy.loginfo("Reached Server")
 	goal = TtsGoal()
 	while not rospy.is_shutdown():
-		text = raw_input("Enter sentence: ")
+		if True:
+			text = "hallo allemaal, welkom"
 		print("---")
 		goal.rawtext.text = text
 		goal.rawtext.lang_id = "NL"
